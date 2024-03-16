92988c037c3a5709b967a0e7c7c63c60bddb54d6
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-03-15 22:33:15 -0300

    adicionou um codigo

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿String palavra;
+Console.WriteLine("--- Soletrar ---");
+Console.Write("digite um texto para se soletrado: ");
+
+palavra = Console.ReadLine ()!;
+Console.WriteLine(string.Join("-",palavra.ToCharArray()));
